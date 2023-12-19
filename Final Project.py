727f04b850c677365dac9a69f9152ca2a8ae64ed
aylahd03 <aylahd3>
2023-12-18 19:15:44 -0500

    first few lines, readme, and license

---
+++ Final Project.py
@@ -0,0 +1,28 @@
+# Final Project
+
+import openaq
+import pandas as pd
+import matplotlib as plt
+
+def openaq_data(api_key, city, parameter, date_from, date_to):
+    aq_data = openaq.OpenAQ(api_key=api_key)
+    
+    #Getting the measurements we want from the api
+    measurements = aq_data.measurements(city=city, parameter=parameter, date_form=date_from, date_to=date_to)
+    
+    # pulling the data we want to use in our visualization
+    data = []
+    for result in measurements ['results']:
+        row = {
+            'date': result['date']['utc'],
+            'value': result['value'],
+            'unit': result['unit']  
+        }
+        data.appen(row) 
+
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,3 @@
+README for INST 126 Final Project
+
+This program is designed to 
