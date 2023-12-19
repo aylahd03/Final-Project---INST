662e3bbef092704be9732c88bcee760c44df56a0
aylahd03 <aylahd3>
2023-12-19 18:42:55 -0500

    hopefully final commit, added comments

--- Final Project.py
+++ Final Project.py
@@ -2,34 +2,39 @@
 
 import requests
 import pandas as pd
+import json
 
-def fetch_user_data():
+# api url to pull data from NASA
+def pull_user_data():
     api_url = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=2023-01-01&end_date=2023-01-07&api_key=vCxBiACkHus3CsVjMd4q98zYTVnTKAUflCB1PQ2z'
 
-    # Make a request to the API
+    # request to the API
     response = requests.get(api_url)
 
-    # Check if the request was successful (status code 200)
+    # check if the request was successful (status code 200)
     if response.status_code == 200:
-        # Parse JSON data from the response
+        # JSON parses data from the response
         data = response.json()
 
-        # Create a Pandas DataFrame from the API response
+        # create a Pandas DataFrame from the API response
         df = pd.DataFrame(data)
         return df
     else:
         print(f"Error: {response.status_code}")
         return None
+ # if request wasn't sucessful it will give back an error
 
 def main():
-    # Fetch user data from the API
-    user_data = fetch_user_data()
+    # pull user data from the API
+    user_data = pull_user_data()
 
     if user_data is not None:
-        # Display the DataFrame
+        # show the DataFrame
         print(user_data)
 
-        # Optionally, perform additional analysis or visualization here
 
 if __name__ == "__main__":
     main()
+
+# I had to do some outside research to understand things like the status code and embedding 
+    # the api url
